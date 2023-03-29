770bfadc95a2a8c088040d46c7f2826801b2bc5d
brunacquaresm <123048801+brunacquaresm@users.noreply.github.com>
2023-03-29 20:25:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+function verificar(event) {
+    event.preventDefault();
+function contar() {
+    let ini = document.getElementById('inicio');
+    let fim = document.getElementById('fim');
+    let passo = document.getElementById('passo');
+    let res = document.getElementById('res');
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossivel contar!'
+    } else {
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            alert('Passo inválido! CONSIDERANDO PASSO 1')
+            p = 1        // Para não travar o navegador.
+        }
+        if(i < f) {
+            // Contagem crescente
+            for(let c = i; c <= f; c += p) {  
+                res.innerHTML += ` ${c} \u{1F449}`
+        }
+        } else {
+            // Contagem regressiva
+            for(let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }
+}
+}
